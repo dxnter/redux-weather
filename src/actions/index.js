67e698f29a2e108f9c2c73c2a0610bc17cd8587a
Danny <contact@danfoster.io>
2018-08-09 21:10:30 -0500

    Added WeatherList component

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '238984bd7c2291e393d7c9491ac01bd2';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+  state = {
+    term: '',
+  };
+
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  };
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
@@ -14,3 +36,8 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+export default connect(
+  null,
+  { fetchWeather }
+)(SearchBar);
